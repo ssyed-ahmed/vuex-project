7f3b1502c781233ef7dcc5b716a652701bf24ef2
unknown <sumair_ahmed4@yahoo.co.in>
2018-06-03 23:21:08 +1000

    Added tut8 changes

--- src/components/ProductListOne.vue
+++ src/components/ProductListOne.vue
@@ -12,19 +12,21 @@
 </template>
 
 <script>
+import { mapActions, mapGetters } from 'vuex'
+
 export default {
   computed: {
     products() {
       return this.$store.state.products
     },
-    saleProducts() {
-      return this.$store.getters.saleProducts
-    }
+    ...mapGetters([
+      'saleProducts'
+    ])
   },
   methods: {
-    reducePrice: function (amount) {
-      this.$store.dispatch('reducePrice', amount)
-    }
+    ...mapActions([
+      'reducePrice'
+    ])
   }
 }
 </script>
